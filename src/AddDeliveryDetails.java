de1fc5ad65e2920410536e75bd20e93016a1be20
Kishlaya Tiwary <armorodoid@gmail.com>
2024-03-13 02:08:54 +0530

    Added Problem Statement and Test File

--- src/AddDeliveryDetails.java
+++ src/AddDeliveryDetails.java
@@ -1,21 +1,33 @@
+import java.io.*;
 import java.util.List;
+import java.util.Scanner;
+
 public class AddDeliveryDetails {
     private static int i;
-    public void addDeliveryDetails(List<DeliveryDetails> pickup) {
-        pickup.add(new DeliveryDetails(new Coordinates(12.934533,77.626579),
-                new Coordinates(12.954533,77.696579),
-                ++i, 10));
-
-        pickup.add(new DeliveryDetails(new Coordinates(12.93,77.8),
-                new Coordinates(12.935,77.69),
-                ++i, 15));
-
-        pickup.add(new DeliveryDetails(new Coordinates(12.95,77.6),
-                new Coordinates(12.937,77.8),
-                ++i, 0));
-
-        pickup.add(new DeliveryDetails(new Coordinates(12.93,77.55),
-                new Coordinates(12.95,77.6),
-                ++i, 5));
+    public int addDeliveryDetails(List<DeliveryDetails> pickup) {
+        int result = 0;
+        try {
+            File myObj = new File("src/tests/test-1.txt");
+            Scanner myReader = new Scanner(myObj);
+            int n = Integer.parseInt(myReader.nextLine());
+            while (i<n) {
+                i++;
+                String str = myReader.nextLine();
+                String[] data = str.split(" ");
+                pickup.add(new DeliveryDetails(
+                        new Coordinates(Double.parseDouble(data[0]),
+                        Double.parseDouble(data[1])),
+                        new Coordinates(Double.parseDouble(data[2]),
+                                Double.parseDouble(data[3])),
+                        i, Integer.parseInt(data[4])));
+                System.out.println(str);
+            }
+            result = Integer.parseInt(myReader.nextLine());
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return result;
     }
 }
